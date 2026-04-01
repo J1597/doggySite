53772c5ae55fe3a4d061c8c7b96096baeafd1f82
Jeevan Gill <jeevangill@msn.com>
2026-04-01 11:06:34 -0700

    Fix css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/styles.css'
+import './styles.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import App from './App.jsx'
 
