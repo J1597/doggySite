0edf9967b5db6e6765fcd88afad8c5aff2db389a
Jeevan Gill <jeevangill@msn.com>
2026-04-06 14:32:56 -0700

    Final polishes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './styles.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
 
 
